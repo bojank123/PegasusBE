07aa996d9b2439f88cb8b2dd15bf1a1dad7ed423
bojank123 <bojan.kozomara@htecgroup.com>
2022-05-18 15:27:26 +0200

    Initial commit

---
+++ src/main/java/com/example/pegasusbe/PegasusBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pegasusbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegasusBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PegasusBeApplication.class, args);
+    }
+
+}
